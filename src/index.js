317b17d2de3d79e75b04afb598aa122dff24cdf3
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-23 11:17:17 -0700

    add rest ability with stoking fire
    Co-authored-by: Gabe Nielsen <gabenielsen7@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,25 +2,31 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { stateControl, gatherFood } from './js/script.js';
+import { stateControl, gatherFood, stokeFire } from './js/script.js';
 
 
 $(document).ready(function() {
-
   $('#gather-food').click(function() {
     let count = parseInt($('#turns').text());
-    console.log(count)
     if (count < 3){
       const newState = stateControl(gatherFood);
-      $('#food-value').text(`Food: ${newState.food}`);
+      $('#food-value').text(`${newState.food}`);
       $('#turns').text(count + 1);
     } else {
       $('#message').text("You must rest to gather food!");
     }
   });
 
-
-  $('#stokeFire').click(function() {
-    
+  $('#rest').click(function() {
+    let count = parseInt($('#turns').text());
+    let woodCount = parseInt($('#wood-value').text());
+    console.log(woodCount);
+    if (woodCount <= 0) {
+      $('#message').text("You must gather more wood!");
+    } else {
+      const newState = stateControl(stokeFire);
+      $('#wood-value').text(`${newState.wood}`)
+      $('#turns').text(count - 1);
+    }
   });
 });
